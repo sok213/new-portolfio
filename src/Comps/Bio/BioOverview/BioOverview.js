cc93c3cc0757335f3f6788b47f3a1bef877a7d88
sok213 <sokcodes@gmail.com>
2021-07-29 22:11:28 -0700

    update bio component

--- src/Comps/Bio/BioOverview/BioOverview.js
+++ src/Comps/Bio/BioOverview/BioOverview.js
@@ -5,9 +5,12 @@ export default function BioOverview() {
 
   return (
     <div className={s.container}>
-      <h1>
-        Bio Overview
+      <p className={s['default-p']}>Hey,</p>
+      <h1 className={s.strongText}>
+        I'm Sophanarith Sok.
       </h1>
+      <h2 className={s.subtext}>It is a long name, so just call me <em>Soak</em>.</h2>
+      <p className={s.textBody}>Officia esse et quis reprehenderit. Proident laboris do voluptate Lorem. Adipisicing ullamco et veniam est amet consectetur. Officia minim labore excepteur aute irure do eu laborum. Ex incididunt exercitation do adipisicing occaecat. Exercitation exercitation nulla pariatur cupidatat dolor non.</p>
     </div>
   )
 }
