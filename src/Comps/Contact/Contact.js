58b25e173f6e8388e9534c4e78a154f01113d089
sok213 <sokcodes@gmail.com>
2021-08-06 00:20:46 -0700

    Update form design

--- src/Comps/Contact/Contact.js
+++ src/Comps/Contact/Contact.js
@@ -18,51 +18,59 @@ export default function Contact() {
     <div className={s.container}>
       <h1>Contact</h1>
 
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">
-          Name
-        </label>
-        <input
-          id="name"
-          type="name" 
-          name="name"
-          required
-        />
-        <ValidationError 
-          prefix="Name" 
-          field="name"
-          errors={state.errors}
-        />
-        <label htmlFor="email">
-          Email Address
-        </label>
-        <input
-          id="email"
-          type="email" 
-          name="email"
-          required
-        />
-        <ValidationError 
-          prefix="Email" 
-          field="email"
-          errors={state.errors}
-        />
-        <label htmlFor="message">
-          Message
-        </label>
-        <textarea
-          id="message"
-          name="message"
-          required
-        />
-        <ValidationError 
-          prefix="Message" 
-          field="message"
-          errors={state.errors}
-        />
-        <button type="submit" disabled={state.submitting}>
-          Submit
-        </button>
+      <form 
+        className={s.formContainer} 
+        onSubmit={handleSubmit}
+      >
+        <div className={s.formContents}>
+          <label htmlFor="email">
+            Name
+          </label>
+          <input
+            id="name"
+            type="name" 
+            name="name"
+            required
+          />
+          <ValidationError 
+            prefix="Name" 
+            field="name"
+            errors={state.errors}
+          />
+          <label htmlFor="email">
+            Email Address
+          </label>
+          <input
+            id="email"
+            type="email" 
+            name="email"
+            required
+          />
+          <ValidationError 
+            prefix="Email" 
+            field="email"
+            errors={state.errors}
+          />
+          <label htmlFor="message">
+            Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            required
+          />
+          <ValidationError 
+            prefix="Message" 
+            field="message"
+            errors={state.errors}
+          />
+
+          <div className={s.buttonContainer}>
+            <button type="submit" disabled={state.submitting}>
+              Submit
+            </button>
+          </div>
+        </div>
       </form>
     </div>
   );
