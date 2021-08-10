57cda1c28e9bde1730632c9699feb0cccba60fa0
sok213 <sokcodes@gmail.com>
2021-08-09 19:17:51 -0700

    Add social links to footer.

--- src/Comps/Footer/Footer.js
+++ src/Comps/Footer/Footer.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import s from './Footer.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 export default function Footer() {
 
   return (
     <div className={s['footer-container']}>
-      <p>Footer goes here.</p>
+      <FontAwesomeIcon icon={faGithub} className={s.link} />
+      <FontAwesomeIcon icon={faLinkedin} className={s.link} />
     </div>
   );
 }
