721979d49117f116e6ae7bfb617e1dba573553a7
sok213 <sokcodes@gmail.com>
2021-08-12 02:08:34 -0700

    Add content to FAQ

--- src/Comps/Bio/BioFAQ/BioFAQ.js
+++ src/Comps/Bio/BioFAQ/BioFAQ.js
@@ -15,27 +15,27 @@ export default function BioFAQ() {
   const faqItems = [
     {
       q: "What's your favourite video game?",
-      a: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '
+      a: 'My favourite casual game is Tetris because it’s a great game that I am able to just jump in and begin having fun within seconds. A more modern and recent game that I really enjoyed was Spider-man: Miles Morales on the PS4 because spiderman is an amazing super-hero and the game had great story and web swinging gameplay.'
     },
     {
       q: "Which holiday do you look forward to the most?",
-      a: 'This is the answer.'
+      a: 'My favourite holiday is Halloween because I love horror movies and, in my opinion, Halloween brings out the best events in terms of musical performers, haunt events such as Halloween Horror Nights at Universal, and the atmosphere that October brings to life.'
     },
     {
       q: 'Are you a sports fan?',
-      a: 'This is the answer.'
+      a: 'I am not the biggest sports fan and if you asked me what is my favourite basketball team, I’d probably tell you that it was the Raiders. However, I do enjoy watching the occasional UFC fight every now and then if I recognize someone who is fighting.'
     },
     {
       q: 'If you could live anywhere, where would it be?',
-      a: 'This is the answer.'
+      a: 'I would live in Finland because it is a very beautiful country and according to Google, it is the happiest place on earth. I would like to see for myself if that is true.'
     },
     {
       q: 'Do you believe in aliens?',
-      a: 'This is the answer.'
+      a: 'Yes. There are trillions and trillions of planets out in the universe and it’s nearly impossible that we are the only living things in existence.'
     },
     {
       q: 'Do you prefer PC or console gaming?',
-      a: 'This is the answer.'
+      a: 'I enjoy both. I have a Playstation 4 and a custom-built desktop computer. There are pros and cons to both. I love the ease-of-use, ready-to-play, and out-of-the-box environment that a console provides to gamers, but also dislike the fact that the hardware will quickly become outdated when compared to PC. I love PC for the customizability of one’s computer and the technical advantages that allow for amazing graphics when compared to consoles, but I also hate that you will be expected to spend an extensive amount of time configuring each game to work properly before getting to jump in and play.'
     }
   ];
 
@@ -76,7 +76,7 @@ export default function BioFAQ() {
     return (
       <div className={s.revealSection}>
         <h2 className={s.subHeader}>QUESTION</h2>
-        <p className={s.textContent}>{q}</p>
+        <p className={s.textContent_Q}>{q}</p>
         <h2 className={s.subHeader}>ANSWER</h2>
 
         <div className={s.textContent_Bottom}>
