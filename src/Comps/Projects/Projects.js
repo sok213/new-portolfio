049cbb62dcf9a78778c47b11b1370645e4761d20
sok213 <sokcodes@gmail.com>
2021-08-12 21:54:18 -0700

    Add icons to project items

--- src/Comps/Projects/Projects.js
+++ src/Comps/Projects/Projects.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import s from './Projects.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGamepad, faBuilding, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 export default function Projects() {
 
@@ -9,6 +11,8 @@ export default function Projects() {
       desc: 'Sint officia veniam et dolore dolore esse ad ut ut magna laboris fugiat sunt consequat. Nulla ad ea ex quis ea elit sit ea. Ea enim officia qui minim incididunt magna qui occaecat duis Lorem et. Nulla culpa irure et id excepteur tempor excepteur consequat.',
       link: '#',
       thumbnail: '../Assets/test.jpg',
+      top: '154px',
+      icon: faBuilding,
       source_code: '#'
     },
     {
@@ -16,6 +20,8 @@ export default function Projects() {
       desc: 'Ea enim officia qui minim incididunt magna qui occaecat duis Lorem et. Nulla culpa irure et id excepteur tempor excepteur consequat. Excepteur mollit laborum voluptate consequat nostrud officia nisi velit',
       link: '#',
       thumbnail: '',
+      icon: faEnvelope,
+      top: '163px',
       source_code: '#',
       width: 150
     },
@@ -24,6 +30,8 @@ export default function Projects() {
       desc: 'Ea enim officia qui minim incididunt magna qui occaecat duis Lorem et. Nulla culpa irure et id excepteur tempor excepteur consequat. Excepteur mollit laborum voluptate consequat nostrud officia nisi velit',
       link: '#',
       thumbnail: '',
+      icon: faGamepad,
+      top: '165px',
       source_code: '#',
       width: 135
     }
@@ -35,7 +43,10 @@ export default function Projects() {
       const {
         title,
         desc,
-        width
+        width,
+        icon,
+        top,
+        left
       } = item;
 
       return (
@@ -44,9 +55,16 @@ export default function Projects() {
             <div className={s.header} style={{ width }} >
               <h3>{title}</h3>
             </div>
+            <div className={s.icon} style={{top, left}}>
+              <FontAwesomeIcon icon={icon} />
+            </div>
           </div>
           <div className={s.desc}>
             <p>{desc}</p>
+            <div className={s.projectLinks}>
+              <p><a href="#">View Project</a></p>
+              <p><a href="#">View Source Code</a></p>
+            </div>
           </div>
         </div>
       );
