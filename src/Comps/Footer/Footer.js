8b486c62e82ddea6919ba9753cd449e18c1535b7
sok213 <sokcodes@gmail.com>
2021-08-19 01:16:30 -0700

    Add facebook icon to footer.

--- src/Comps/Footer/Footer.js
+++ src/Comps/Footer/Footer.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import s from './Footer.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedin, faFacebookSquare } from '@fortawesome/free-brands-svg-icons';
 
 export default function Footer() {
 
   return (
     <div className={s['footer-container']}>
+      <a 
+        href="https://www.linkedin.com/in/sophanarith-sok-a50097b7/"
+        target="_blank"
+      >
+        <FontAwesomeIcon 
+          icon={faLinkedin} 
+          className={s.link} 
+        />
+      </a>
       <a 
         href="https://github.com/sok213"
         target="_blank"
@@ -17,11 +26,11 @@ export default function Footer() {
         />
       </a>
       <a 
-        href="https://www.linkedin.com/in/sophanarith-sok-a50097b7/"
+        href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
         target="_blank"
       >
         <FontAwesomeIcon 
-          icon={faLinkedin} 
+          icon={faFacebookSquare} 
           className={s.link} 
         />
       </a>
