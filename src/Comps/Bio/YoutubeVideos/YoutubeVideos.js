ffee542627693e8e4e5e61b1612b51fd05116658
sok213 <sokcodes@gmail.com>
2023-04-15 04:08:59 -0700

    Update responsivenss for youtube video.

--- src/Comps/Bio/YoutubeVideos/YoutubeVideos.js
+++ src/Comps/Bio/YoutubeVideos/YoutubeVideos.js
@@ -4,9 +4,11 @@ import s from './YoutubeVideos.module.scss';
 export default function YoutubeVideos() {
 
   return (
-    <div className={s.container}>
+    <Fragment>
       <h2 className={s.header}>Portfolio Walkthrough</h2>
-      <iframe src="https://www.youtube.com/embed/acZ_qoYscw8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-    </div>
+      <div className={s.iFrameContainer}>
+        <iframe src="https://www.youtube.com/embed/acZ_qoYscw8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      </div>
+    </Fragment>
   );
 }
