de06b336fd8c9fb05b37eb65098a3cd765e6632e
sok213 <sokcodes@gmail.com>
2023-04-15 21:16:46 -0700

    Add project deep dive video

--- src/Comps/Bio/YoutubeVideos/YoutubeVideos.js
+++ src/Comps/Bio/YoutubeVideos/YoutubeVideos.js
@@ -9,6 +9,11 @@ export default function YoutubeVideos() {
       <div className={s.iFrameContainer}>
         <iframe src="https://www.youtube.com/embed/acZ_qoYscw8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </div>
+
+      <h2 className={s.header}>Project Deep Dive</h2>
+      <div className={s.iFrameContainer}>
+        <iframe src="https://www.youtube.com/embed/-Ib5q8gdhLQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      </div>
     </Fragment>
   );
 }
