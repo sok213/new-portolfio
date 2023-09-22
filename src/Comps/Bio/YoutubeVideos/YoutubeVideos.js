b98c79f6baa5ba0f4b1be8aa19c57c0b795610e2
sok213 <sokcodes@gmail.com>
2023-09-21 19:38:05 -0700

    Update video link

--- src/Comps/Bio/YoutubeVideos/YoutubeVideos.js
+++ src/Comps/Bio/YoutubeVideos/YoutubeVideos.js
@@ -7,13 +7,8 @@ export default function YoutubeVideos() {
     <Fragment>
       <h2 className={s.header}>Portfolio Walkthrough</h2>
       <div className={s.iFrameContainer}>
-        <iframe src="https://www.youtube.com/embed/acZ_qoYscw8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe src="https://www.youtube.com/embed/998yAMPrLDg?si=vGsWTEGbKVZYdnO8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </div>
-
-      {/* <h2 className={s.header}>Project Deep Dive</h2>
-      <div className={s.iFrameContainer}>
-        <iframe src="https://www.youtube.com/embed/-Ib5q8gdhLQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-      </div> */}
     </Fragment>
   );
 }
