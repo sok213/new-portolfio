3b4dc19b374755f681e3e804b11bf01565a525ef
Sophanarith Sok <sokcodes@gmail.com>
2025-02-27 22:01:04 -0800

    update youtube video

--- src/Comps/Bio/YoutubeVideos/YoutubeVideos.js
+++ src/Comps/Bio/YoutubeVideos/YoutubeVideos.js
@@ -7,7 +7,7 @@ export default function YoutubeVideos() {
     <Fragment>
       <h2 className={s.header}>Portfolio Walkthrough</h2>
       <div className={s.iFrameContainer}>
-        <iframe src="https://www.youtube.com/embed/998yAMPrLDg?si=vGsWTEGbKVZYdnO8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe src="https://www.youtube.com/embed/JPJ9gKAkrcU?si=s6WsWr1ibqo04Ta9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </div>
     </Fragment>
   );
